29d042975721f0aa4113c7d320151a804cf5e5c1
tahirhakak <tahirpc7@gmail.com>
2025-05-13 20:37:08 +0530

    Add files via upload

---
+++ calculator python.py
@@ -0,0 +1,295 @@
+print("Coder: Mohammad Tahir")
+name = input("Hello User, Please Enter Your Name: ")
+print('Welcome:',name)
+
+import os
+import time
+time.sleep(2)
+os.system('cls')
+
+print("1. Add \n"
+      "2. Sub \n"
+      "3. Multiply \n"
+      "4. Divide \n \n"
+      "5. My Github Link")
+
+select = int(input("Enter 1-5: "))
+# if user select 1
+if select >= 6:
+  print("Error, You Entered Wrong Number")
+if select == 1:
+
+    print("You Entered 1:")
+    time.sleep(2)
+    os.system('cls')
+    num1 = float(input("1st Number: "))
+    num2 = float(input("2nd Number: "))
+    t1 = num1 + num2 
+    print("Ans:",t1)
+    time.sleep(2)
+    os.system('cls')
+    print("Ans:",t1)
+    print("1. Add \n"
+      "2. Sub \n"
+      "3. Multiply \n"
+      "4. Divide \n \n"
+      "5. My Github Link")
+    select = int(input("Enter 1-5: "))
+
+    
+    # if user select 1 : sub 
+    if select >= 6:
+     print("Error, You Entered Wrong Number")
+    if select == 1:
+        print("You Entered 1:")
+        time.sleep(2)
+        os.system('cls')
+        print("Ans:",t1)
+        num3 = float(input("3rd Number : "))
+        print("Ans:",t1 + num3)
+        print("Now Software Will Close Automatically in 7 sec...")
+        time.sleep(7)
+ 
+    elif select == 2:
+     print("You Entered 2:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 - num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 3:
+     print("You Entered 3:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 * num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 4:
+     print("You Entered 4:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 / num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 5:
+      print("You Entered 5:")
+      print("My Github Link: https://github.com/tahirhakak")
+      exit()
+
+# If user select 2
+
+if select == 2:
+    print("You Entered 2:")
+    time.sleep(2)
+    os.system('cls')
+    num1 = float(input("1st Number: "))
+    num2 = float(input("2nd Number: "))
+    t1 = num1 - num2 
+    print("Ans:",t1)
+    time.sleep(2)
+    os.system('cls')
+    print("Ans:",t1)
+    print("1. Add \n"
+      "2. Sub \n"
+      "3. Multiply \n"
+      "4. Divide \n \n"
+      "5. My Github Link")
+    select = int(input("Enter 1-5: "))
+    # if user select 2 : sub 
+    if select >= 6:
+     print("Error, You Entered Wrong Number")
+    if select == 1:
+        print("You Entered 1:")
+        time.sleep(2)
+        os.system('cls')
+        print("Ans:",t1)
+        num3 = float(input("3rd Number : "))
+        print("Ans:",t1 + num3)
+        print("Now Software Will Close Automatically in 7 sec...")
+        time.sleep(7)
+ 
+    elif select == 2:
+     print("You Entered 2:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 - num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 3:
+     print("You Entered 3:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 * num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 4:
+     print("You Entered 4:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 / num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 5:
+      print("You Entered 5:")
+      print("My Github Link: https://github.com/tahirhakak")
+      exit()
+
+# If user select 3
+
+if select == 3:
+    print("You Entered 3:")
+    time.sleep(2)
+    os.system('cls')
+    num1 = float(input("1st Number: "))
+    num2 = float(input("2nd Number: "))
+    t1 = num1 * num2 
+    print("Ans:",t1)
+    time.sleep(2)
+    os.system('cls')
+    print("Ans:",t1)
+    print("1. Add \n"
+      "2. Sub \n"
+      "3. Multiply \n"
+      "4. Divide \n \n"
+      "5. My Github Link")
+    select = int(input("Enter 1-5: "))
+   
+    # if user select 3 : sub 
+    if select >= 6:
+     print("Error, You Entered Wrong Number")
+    if select == 1:
+        print("You Entered 1:")
+        time.sleep(2)
+        os.system('cls')
+        print("Ans:",t1)
+        num3 = float(input("3rd Number : "))
+        print("Ans:",t1 + num3)
+        print("Now Software Will Close Automatically in 7 sec...")
+        time.sleep(7)
+ 
+    elif select == 2:
+     print("You Entered 2:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 - num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 3:
+     print("You Entered 3:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 * num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 4:
+     print("You Entered 4:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 / num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 5:
+      print("You Entered 5:")
+      print("My Github Link: https://github.com/tahirhakak")
+      exit()
+
+# If user select 4
+
+if select == 4:
+    print("You Entered 4:")
+    time.sleep(2)
+    os.system('cls')
+    num1 = float(input("1st Number: "))
+    num2 = float(input("2nd Number: "))
+    t1 = num1 / num2 
+    print("Ans:",t1)
+    time.sleep(2)
+    os.system('cls')
+    print("Ans:",t1)
+    print("1. Add \n"
+      "2. Sub \n"
+      "3. Multiply \n"
+      "4. Divide \n \n"
+      "5. My Github Link")
+    select = int(input("Enter 1-5: "))
+   
+    # if user select 4 : sub 
+    if select >= 6:
+     print("Error, You Entered Wrong Number")
+    if select == 1:
+        print("You Entered 1:")
+        time.sleep(2)
+        os.system('cls')
+        print("Ans:",t1)
+        num3 = float(input("3rd Number : "))
+        print("Ans:",t1 + num3)
+        print("Now Software Will Close Automatically in 7 sec...")
+        time.sleep(7)
+ 
+    elif select == 2:
+     print("You Entered 2:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 - num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 3:
+     print("You Entered 3:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 * num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 4:
+     print("You Entered 4:")
+     time.sleep(2)
+     os.system('cls')
+     print("Ans:",t1)
+     num3 = float(input("3rd Number : "))
+     print("Ans:",t1 / num3)
+     print("Now Software Will Close Automatically in 7 sec...")
+     time.sleep(7)
+
+    elif select == 5:
+      print("You Entered 5:")
+      print("My Github Link: https://github.com/tahirhakak")
+      exit()
+
+# If user select 5
+
+if select == 5:
+  print("My Github Link: https://github.com/tahirhakak")
+  exit()
